987d7a0c12be9923303c48dc93b41e8fa76897c3
Joseph <goldenprince724@gmail.com>
2023-02-05 13:53:16 +0000

    Added Authentication

---
+++ middlewares/authentication.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const customError = require('http-errors')
+
+
+const auth = async (req, res, next) => {
+
+    const authHeader = req.headers.authorization
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new customError.Unauthorized('Authentication Invalid')
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const payload = await jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = {userId:payload.userId, name:payload.name}
+        next()
+    } catch (error) {
+        throw new customError.Unauthorized('Authentication Invalid')
+    }
+}
+
+module.exports = auth
